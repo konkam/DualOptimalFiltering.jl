9287dfc0cbb7285761194e989cd0c508fb014b5b
konkam <guillaume.konkamking.work@gmail.com>
2020-01-29 17:38:24 +0100

    new test on travis

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
-using Pkg
+# using Pkg
 println("I am being built...")
-Pkg.add(PackageSpec(url="https://github.com/konkam/ExactWrightFisher.jl"))
-Pkg.add(PackageSpec(url="https://github.com/konkam/FeynmanKacParticleFilters.jl"))
+# Pkg.add(PackageSpec(url="https://github.com/konkam/ExactWrightFisher.jl"))
+# Pkg.add(PackageSpec(url="https://github.com/konkam/FeynmanKacParticleFilters.jl"))

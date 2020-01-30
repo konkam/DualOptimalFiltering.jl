62bb8120b859b4889b80b4e4667ed6ce713e8290
konkam <guillaume.konkamking.work@gmail.com>
2020-01-30 18:26:24 +0100

    added a print of the acceptance rate

--- src/mcmc_sampler.jl
+++ src/mcmc_sampler.jl
@@ -17,7 +17,7 @@ function draw_next_sample(state::T, Jtsym_rand::Function, unnormalised_logposter
     end
 end
 
-function get_mcmc_samples_bare(nsamp, starting_state, Jtsym_rand_create::Function, unnormalised_logposterior::Function; silence = false)
+function get_mcmc_samples_bare(nsamp, starting_state, Jtsym_rand_create::Function, unnormalised_logposterior::Function; silence = false, print_acceptance_rate = false)
     chain = Array{typeof(first(starting_state)), 2}(undef, length(starting_state), nsamp+1)
     chain[:,1] = starting_state
 
@@ -30,11 +30,14 @@ function get_mcmc_samples_bare(nsamp, starting_state, Jtsym_rand_create::Functio
         end
         chain[:,i] = draw_next_sample(chain[:,i-1], Jtsym_rand, unnormalised_logposterior)
     end
+    if print_acceptance_rate
+        println((length(unique(chain[1,:]))-1)/nsamp)
+    end
     return chain
 end
 
-get_mcmc_samples(nsamp, starting_state, Jtsym_rand_create::Function, unnormalised_logposterior::Function; warmup_percentage = 0.1, final_size = Inf, silence = false) =
-get_mcmc_samples_bare(nsamp, starting_state, Jtsym_rand_create, unnormalised_logposterior, silence = silence) |> c -> discard_warmup(c, warmup_percentage) |> c -> thin_chain(c, final_size)
+get_mcmc_samples(nsamp, starting_state, Jtsym_rand_create::Function, unnormalised_logposterior::Function; warmup_percentage = 0.1, final_size = Inf, silence = false, print_acceptance_rate = false) =
+get_mcmc_samples_bare(nsamp, starting_state, Jtsym_rand_create, unnormalised_logposterior, silence = silence, print_acceptance_rate = print_acceptance_rate) |> c -> discard_warmup(c, warmup_percentage) |> c -> thin_chain(c, final_size)
 
 function discard_warmup(chain, percentage)
     idcut = Int64(round(size(chain, 2) * percentage))
